4f90baa7e62a6a48d6d4eae3698fd1d05b8a01c8
Rejn2024 <r.edward.j.nicklin@gmail.com>
2026-07-08 13:18:23 +0100

    Add radar type classification task

--- rgcn_fusion/__init__.py
+++ rgcn_fusion/__init__.py
@@ -1,10 +1,11 @@
 """r-GCN based Dempster-Shafer fusion utilities."""
 
 from .dempster_shafer import belief_plausibility, combine_masses, validate_masses
-from .model import RGCNEvidenceModel
+from .model import RGCNEvidenceModel, RGCNLayer
 
 __all__ = [
     "RGCNEvidenceModel",
+    "RGCNLayer",
     "belief_plausibility",
     "combine_masses",
     "validate_masses",
